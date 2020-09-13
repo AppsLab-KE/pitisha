67be26d1f5bdefc0e6e05154ef1a2abf792c46f6
kodaTheCoder <marvincollins14@gmail.com>
2020-09-13 16:47:58 +0300

    clean up

--- src/ApprovableServiceProvider.php
+++ src/ApprovableServiceProvider.php
@@ -19,7 +19,7 @@ class ApprovableServiceProvider extends ServiceProvider
      */
     private function registerPublishing()
     {
-        //this is to allow you to modify the tables according to your project need
+        //this is to allow you to modify the tables and config according to your project need
         $this->publishes([
             __DIR__ . '/../config/pitisha.php' => 'config/pitisha.php',
             __DIR__ . '/../database/migrations/2018_10_12_000000_create_approvals_table.php' =>
